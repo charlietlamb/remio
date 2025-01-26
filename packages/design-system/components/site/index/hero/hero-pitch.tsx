dd1ab0fb507697e6bb403d6736df5ecdee807dd5
Charlie Lamb <charlielamb20@icloud.com>
2025-01-26 09:59:23 +0000

    add image locally

--- packages/design-system/components/site/index/hero/hero-pitch.tsx
+++ packages/design-system/components/site/index/hero/hero-pitch.tsx
@@ -1,14 +1,14 @@
-import { env } from '@remio/env'
 import Image from 'next/image'
 
 export default function HeroPitch() {
   return (
-    <img
+    <Image
       alt="SaaS Dashboard"
-      src={`${env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/public/remio/demo.webp`}
+      src={`/demo.webp`}
       width={1000}
       height={698}
       className="rounded-xl border border-border shadow-lg md:w-[80%] w-full"
+      priority
     />
   )
 }
